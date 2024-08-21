08d8c077d33fe01f4843bdeb4460362b447959a8
Augusto Schweitzer <augustoschweitzer2@gmail.com>
2024-08-21 19:40:11 -0300

    package respostas criado para lidar com erros

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -4,10 +4,9 @@ import (
 	"api/src/banco"
 	"api/src/modelos"
 	"api/src/repositorios"
+	"api/src/respostas"
 	"encoding/json"
-	"fmt"
 	"io"
-	"log"
 	"net/http"
 )
 
@@ -15,29 +14,33 @@ import (
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	corpoRequest, erro := io.ReadAll(r.Body)
 	if erro != nil {
-		log.Fatal(erro) // Todos os tratamentos de erro em "usuarios.go" são temporarios, e serão tratados de forma adequeda no futuro
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
 	}
 
 	var usuario modelos.Usuario
 
 	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil { // Transforma a request em uma variavel do tipo Usuario
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
 	}
 
 	db, erro := banco.Conectar() // Abre a conexão com o banco de dados
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
 	}
 	defer db.Close() //	No fim da função, fecha a conexão com o banco de dados
 
 	repositorio := repositorios.NovoRepositorioDeUsuarios(db) // Adiciona o banco no repositorio
 	usuarioID, erro := repositorio.Criar(usuario)             // Lida com a lógica de adicionar um usuário no banco
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
 	}
 
-	w.Write([]byte(fmt.Sprintf("ID inserido: %d", usuarioID)))
-
+	usuario.ID = usuarioID
+	respostas.JSON(w, http.StatusCreated, usuario)
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
--- api/src/modelos/Usuario.go
+++ api/src/modelos/Usuario.go
@@ -2,13 +2,12 @@ package modelos
 
 import "time"
 
-
 // Usuario representa uma struct de um usuário que possui todas as suas informações pessoais
 type Usuario struct {
-	ID       uint64 `json:"id,omitempty"`
-	Nome     string `json:"nome,omitempty"`
-	Nick     string `json:"nick,omitempty"`
-	Email    string `json:"email,omitempty"`
-	Senha    string `json:"senha,omitempty"`
+	ID       uint64    `json:"id,omitempty"`
+	Nome     string    `json:"nome,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"CriadoEm,omitempty"`
 }
