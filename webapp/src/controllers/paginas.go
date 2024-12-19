6791e3f85306d6c154c3c113912d875686976862
Augusto Schweitzer <augustoschweitzer2@gmail.com>
2024-12-18 21:44:03 -0300

    pagina de cadastro e jquery

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -9,3 +9,8 @@ import (
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "login.html", nil)	// nil pq não temos dados para passar para o template
 }
+
+
+func CarregarTelaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "cadastro.html", nil)
+}
